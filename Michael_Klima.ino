504eb622a48f4f8dfab3ec4e676d954c77b8080e
Michael Hufschmidt <michael@hufschmidt-web.de>
2022-04-29 18:47:03 +0200

    now using the internal web-server

--- Michael_Klima.ino
+++ Michael_Klima.ino
@@ -1,22 +1,23 @@
 /*****************************************************************************
    File:              Michael_Klima.ino, Version 1.0
    Created:           2021-12-17
-   Last modification: 2021-12-23
-   Program size:      Sketch 271033 Bytes (25%), Globals 28624 Bytes (34%)
+   Last modification: 2022-02-24
+   Program size:      Sketch 315537 Bytes (30%), Global Vars 31264 Bytes (38%)
    Author and (C):    Michael Hufschmidt <michael@hufschmidt-web.de>
    License:           https://creativecommons.org/licenses/by-nc-sa/3.0/de/
  * ***************************************************************************/
-/* Michaels Raumklima-Monitor. Inspiriert duch den Artikel IKEA Vindiktning
-   hacken, siehe Make 5/2021, Seite 14 ff. Es ist geplant, weitere Sensoren
-   anzuschließen.
-   Im Original: VINDRIKTNING Ikea Luftgüte Sensor -> AdafruitIO
-  
+/* Michaels Raumklima-Monitor. Inspiriert durch den Artikel "IKEA Vindiktning
+   hacken", siehe Make 5/2021, Seite 14 ff. Es ist geplant, weitere Sensoren
+   anzuschliessen.
+   Im Original: VINDRIKTNING Ikea Luftguete Sensor -> AdafruitIO
+
    ***** Computer: Wemos D1 Mini ESP8266
    https://www.manualslib.com/manual/1800815/Wemos-D1-Mini.html
    https://www.espressif.com/sites/default/files/documentation/0a-esp8266ex_datasheet_en.pdf
    https://www.espressif.com/sites/default/files/documentation/esp8266-technical_reference_en.pdf
 
-   ***** Quellen für die Libraries und übernommene Code-Blöcke:
+   ***** Quellen für die Libraries und uebernommene Code-Bloecke:
+   https://blog.rolandbaer.ch/2020/03/30/wemos-d1-mini-als-temperatur-und-luftfeuchtigkeits-webserver/
    Serial Code: https://github.com/Hypfer/esp8266-vindriktning-particle-sensor
    Examples: https://github.com/wemos/D1_mini_Examples
    ESP8266:  https://github.com/esp8266/Arduino/tree/master/libraries/ESP8266WiFi/src
@@ -26,7 +27,23 @@
    DS18B20:  https://github.com/milesburton/Arduino-Temperature-Control-Library/blob/master/DallasTemperature.h
    DHT11:    https://cdn-learn.adafruit.com/downloads/pdf/dht.pdf
              https://github.com/adafruit/DHT-sensor-library
+   MQTT:     https://github.com/knolleary/pubsubclient
+   *** TODO: add MQTT functionality
  * ***************************************************************************/
+
+// ***** Includes
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <SoftwareSerial.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <DHT.h>
+// #include "EspMQTTClient.h"
+#include <PubSubClient.h>
+
+// ***** External constants and variables
+#include "_private.h"
+
 
 // ***** Pin definitions
 #ifdef LED_BUILTIN                                  // true for D1 Mini
@@ -37,29 +54,91 @@
   #define AN HIGH
   #define AUS LOW
 #endif
-// #define MAKER_MAGAZIN
-#ifdef  MAKER_MAGAZIN       // Original-version aus Make: 05/21
-  constexpr static const uint8_t PIN_UART_RX = D2;  // =GPIO4 am Wemos D1 Mini
-#else                       // Version für mein Adapter-Board V1.x
-  constexpr static const uint8_t PIN_UART_RX = D5;  // =GPIO14 am Wemos D1 Mini
-#endif
+// Original-Version aus Make 05/21:
+// constexpr static const uint8_t PIN_UART_RX = D2; // =GPIO4 am Wemos D1 Mini
+// Version fuer mein Adapter-Board:
+constexpr static const uint8_t PIN_UART_RX = D5;    // =GPIO14 am Wemos D1 Mini
 constexpr static const uint8_t PIN_UART_TX = D7;    // =GPIO13, UNUNSED
 constexpr static const uint8_t ONE_WIRE_BUS = D6;   // =GPIO12 am Wemos D1 Mini
 
-// ***** Includes
-#include <SoftwareSerial.h>
-#include <OneWire.h>
-#include <DallasTemperature.h>
-#include <DHT.h>
 
 // ***** Constants (alle Zeiten in Millisekunden)
 const uint8_t BLINK_COUNT = 3;  // Default Anzahl Lichtblitze
 const uint16_t PUNKT = 200;     // Punktlaenge (Norm = 100 ms bei 60 BpM)
 const uint16_t ABSTAND = 200;   // Punkt-Strich Abstand (Norm = 100 ms)
 const uint16_t ZEICHEN = 1000;  // Buchstaben-Abstand im Wort (Norm = 3x Punkt)
+//* Set these to your desired credentials. */
+const char *extSsid = WIFI_SSID;
+const char *extPassword = WIFI_PASS;
+const char *mySsid = APSSID;
+const char *myPassword = APPSK;
+const char* mqtt_server = MQTT_BROKER;
+
+
+// ***** Variables
+String localIP_s, macAddress_s, externalIP_s;
+bool wlanOK = false;
+typedef struct {
+  String param;
+  String value;
+  String unit;
+  unsigned long measurement;
+  unsigned long runID;
+  String topic;
+  bool active;
+} sensor_type;
+#ifdef ZU_HAUSE
+  sensor_type sensor[] = {
+    {"Feinstaub [Vindriktning]", "0", "µg/m³", 0, 0,
+     "ZuHause/Wohnzimmer/Feinstaub", true},
+    {"Temperatur [DHT 11]", "0.0", "°C", 0, 0,
+     "ZuHause/Wohnzimmer/Temperatur_int", true},
+    {"Luftfeuchte [DHT 11]", "0.0", "%", 0, 0,
+     "ZuHause/Wohnzimmer/Luftfeuchte", true},
+    {"Temperatur [DS 18x20 #0]", "0.0", "°C", 0, 0,
+     "ZuHause/Wohnzimmer/Temperatur_ext", true},
+    {"Temperatur [DS 18x20 #1]", "0.0", "°C", 0, 0, "", true},
+    {"CO₂ - Gehalt [SCD 30]", "0.0", "ppm", 0, 0, "", false}
+  };
+#endif
+#ifdef IM_INSTITUT
+  sensor_type sensor[] = {
+    {"Feinstaub [Vindriktning]", "0", "µg/m³", 0, 0, "", true},
+    {"Temperatur [DHT 11]", "0.0", "°C", 0, 0, "", false},
+    {"Luftfeuchte [DHT 11]", "0.0", "%", 0, 0, "", false},
+    {"Temperatur [DS 18x20 #0]", "0.0", "°C", 0, 0, "", true},
+    {"Temperatur [DS 18x20 #1]", "0.0", "°C", 0, 0, "", true},
+    {"CO₂ - Gehalt [SCD 30]", "0.0", "ppm", 0, 0, "", false}
+  };
+#endif
+// ***** TODO:
+// Als Klasse deklarierien, getSensorData() als Methode implementieren
+int sensorCount = sizeof(sensor) / sizeof(sensor_type);
+uint8_t deviceCount = 0;            // Anzahl der OneWire - Clients
+uint8_t dallasCount = 0;            // Anzahl der DS18x10 - Sensoren
+DeviceAddress tempAddr, ds1820_0, ds1820_1;     // DS 18x20 Sensoren
+uint8_t serialRxBuf[80];
+uint8_t rxBufIdx = 0;
+unsigned long runID = 0;
+unsigned long previousMillis = 0;
+char msg[MSG_BUFFER_SIZE];
+unsigned long lastMsg = 0;
+
+
+// ***** Objects
+SoftwareSerial sensorSerial(PIN_UART_RX, PIN_UART_TX);
+ESP8266WebServer webServer(80);
+WiFiClient espClient;
+PubSubClient client(espClient);
+OneWire oneWire(ONE_WIRE_BUS);
+DHT dht(ONE_WIRE_BUS, DHTTYPE, 6);
+DallasTemperature ds1820(&oneWire);
+
 
 // ***** Functions
 void blink(const uint8_t count = BLINK_COUNT) {
+  digitalWrite(LED_BUILTIN, AUS);
+  delay(ZEICHEN);
   for (uint8_t i = 0; i < count; i++) {
     digitalWrite(LED_BUILTIN, AN);
     delay(ABSTAND);
@@ -67,45 +146,327 @@ void blink(const uint8_t count = BLINK_COUNT) {
     delay(ABSTAND);
   }
   delay(ZEICHEN);
-}
+} // blink()
 
-// ***** Objects
-SoftwareSerial sensorSerial(PIN_UART_RX, PIN_UART_TX);
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature ds1820(&oneWire);
-DHT dht11(ONE_WIRE_BUS, DHT11);
+String deviceAddressToString(DeviceAddress a) {
+  char buffer[32];
+  snprintf(buffer, 30, "%0x %0x %0x %0x  %0x %0x %0x %0x",
+           a[7], a[6], a[5], a[4], a[3], a[2], a[1], a[0]);
+  return String(buffer);
+} // deviceAddressToString(...)
 
-// ***** Variables
-uint8_t deviceCount = 0;     // Anzahl der OneWire - Clients
-uint8_t dallasCount = 0;     // Anzahl der DS18x10 - Sensoren
-float temperature;           // hier wird die Temperatur gespeichert
-uint8_t serialRxBuf[80];
-uint8_t rxBufIdx = 0;
-unsigned long previousMillis = 0;
+void connectWiFi() {
+  unsigned long now =  millis();
+  unsigned long until = now + 1000 * wlanTimeout;
+  if (wlanOK) return;
+  // connecting to  external WLAN network
+  // https://arduino-esp8266.readthedocs.io/en/3.0.2/esp8266wifi/readme.html
+  Serial.printf("Connecting to external WLAN SSID %s with Password %s\n",
+                extSsid, extPassword);
+  WiFi.mode(WIFI_AP_STA);                       // this is the default
+  WiFi.begin(extSsid, extPassword);
+  // check wi-fi staus until connected
+  while (WiFi.status() != WL_CONNECTED && millis() < until) {
+    delay(1000);
+    Serial.print(".");
+  }
+  if (millis() < until) {
+    wlanOK = true;
+    externalIP_s = WiFi.localIP().toString();
+    Serial.printf("\nExternal WLAN SSID %s connected, ext. IP address = %s\n",
+                  extSsid, externalIP_s.c_str());
+    // WiFi.printDiag(Serial);  // for Diagnosticts
+  } else {
+    // softAPdisconnect(wifioff);
+    Serial.printf("\nExternal WLAN SSID %s, Timeout after %d s\n",
+	              extSsid, wlanTimeout);
+  }
+  return;
+}  // connectWiFi()
 
-// ***** vaiables and constants, -> external
-#include "_private.h"
+String runInfo() {
+  unsigned long currentMillis, d, h, m, s;
+  currentMillis = millis();
+  s = currentMillis / 1000;
+  m = s / 60;
+  h = m / 60;
+  d = h / 24;
+  String info = "Run #";
+  info += String(runID);
+  info += ", Uptime: " + String(d) + " d, ";
+  info += String(h % 24) + " h, ";
+  info += String(m % 60) + " m, ";
+  info += String(s % 60) + " s ";
+  return info;
+}  // runInfo()
 
-void setup() {                                      // put your setup code here, to run once
+int getIkeaData() {  // from the Make Magazin
+  uint8_t rxBufIdx = 0;
+  uint8_t checksum = 0;
+  // Sensor Serial aushorchen
+  while ((sensorSerial.available() && rxBufIdx < 127) ||
+         rxBufIdx < 20) {
+    serialRxBuf[rxBufIdx++] = sensorSerial.read();
+    // Wenn es Probleme mit dem Empfang gibt:
+    delay(15);
+  }
+  // Pruefsumme berechnen
+  for (uint8_t i = 0; i < 20; i++)  {
+    checksum += serialRxBuf[i];
+  }
+  // Header und Pruefsumme checken
+  if (serialRxBuf[0] == 0x16 && serialRxBuf[1] == 0x11 &&
+      serialRxBuf[2] == 0x0B && checksum == 0) {
+    return (serialRxBuf[5] << 8 | serialRxBuf[6]);
+  } else {
+    return (-1);
+  }
+} // getIkeaData()
+
+void getSensorData() {
+  runID += 1;
+  bool ok;
+  float temperature;
+  int feinstaub;
+  if (sensor[0].active) {
+    #ifdef TEST_MODE  // Michael Hufschmidt, 2022-02-23
+      feinstaub = random(20, 50);
+    #else
+      feinstaub = getIkeaData();
+    #endif
+    //  delay(1000);
+    if (feinstaub > 0) {
+      sensor[0].measurement += 1;
+      sensor[0].runID = runID;
+      sensor[0].value = String(feinstaub);
+    } // feinstaub > 0
+  }  // sensor[0].active
+  if (sensor[1].active) {
+    if (!isnan(dht.readTemperature())) {
+      sensor[1].measurement += 1;
+      sensor[1].runID = runID;
+      sensor[1].value = String(dht.readTemperature(), 1);
+    }
+  }  // sensor[1].active
+  if (sensor[2].active) {
+    if (!isnan(dht.readHumidity())) {
+      sensor[2].measurement += 1;
+      sensor[2].runID = runID;
+      sensor[2].value = String(dht.readHumidity(), 1);
+    }
+  }  // sensor[2].active
+  if (sensor[3].active) {
+    delay(1000);
+    ds1820.requestTemperatures();
+    //  ok = ds1820.requestTemperaturesByAddress(ds1820_0);
+    //  ok = ds1820.requestTemperaturesByIndex(0);
+    temperature = ds1820.getTempC(ds1820_0);
+    //  temperature = ds1820.getTempCByIndex(0);
+    if (temperature != DEVICE_DISCONNECTED_C) {
+      // see DallasTemperature.h line 36
+      sensor[3].measurement += 1;
+      sensor[3].runID = runID;
+      sensor[3].value = String(temperature, 1);
+    } else {
+      ;
+      //    sensor[3].value = "???";
+    }  // if
+  }  // sensor[3].active
+  if (sensor[4].active) {
+    delay(1000);
+    ds1820.requestTemperatures();
+    //  ok = ds1820.requestTemperaturesByAddress(ds1820_1);
+    //  ok = ds1820.requestTemperaturesByIndex(1);
+    temperature = ds1820.getTempC(ds1820_1);
+    //  temperature = ds1820.getTempCByIndex(1);
+    if (temperature != DEVICE_DISCONNECTED_C) { // DallasTemperature.h line 36
+      sensor[4].measurement += 1;
+      sensor[4].runID = runID;
+      sensor[4].value = String(temperature, 1);
+    } else {
+      ;
+      //    sensor[4].value = "???";
+    }  // if
+  } // sensor[4].active
+  if (sensor[5].active) {
+    ;
+  }  // sensor[5].active
+}  // getSensorData()
+
+void publishSensorData() {
+  for (uint8_t i = 0; i < sensorCount; i++) {
+    if (sensor[i].active) {
+      ;
+    }  // if
+  }  // for
+}  // publishSensorData()
+
+String formatSensorData(bool html = false) {
+  String out = "";
+  out += html ? "<p><br>" : "\n";
+  out += runInfo();
+  out += html ? "</p> \r\n" : "\n";
+  for (uint8_t i = 0; i < sensorCount; i++) {
+    if (sensor[i].active) {
+      out += html ? "<p>" : "";
+      out += sensor[i].param + ": " + sensor[i].value + " " + sensor[i].unit;
+      out += " (Messung #" + String(sensor[i].measurement);
+      out += " in Run #" + String(sensor[i].runID) + ")";
+      out += html ? "</p> \r\n" : "\n";
+    } // if
+  } // for i
+  return out;
+}  // formatSensorData()
+
+void printSensorData() {
+  Serial.print(formatSensorData());
+}  // printSensorData()
+
+void handleRoot() {
+  /* Just a little test message.  Go to http://192.168.4.1 in a
+     web browser connected to this access point to see it. */
+  webServer.send(200, "text/html", "<h1>You are connected</h1>");
+}  // handleRoot()
+
+void handle_OnConnect() {
+  // getSensorData();
+  webServer.send(200, "text/html", buildHtml());
+}  // handle_OnConnect()
+
+void handle_NotFound() {
+  webServer.send(404, "text/plain", "Not found");
+}  // handle_NotFound()
+
+String buildHtml() {
+  String page = "<!DOCTYPE HTML PUBLIC ";
+  page += "\"-//W3C//DTD HTML 4.01 Transitional//EN\" ";
+  page += "\"http://www.w3.org/TR/html4/loose.dtd\"> \r\n";
+  page += "<html>\r\n";
+  page += "<head>\r\n";
+  page += "<title>" + title + "</title>\r\n";
+  page += "<meta name=\"viewport\"";
+  page +=   " content=\"width=device-width, initial-scale=1.0,";
+  page +=   " user-scalable=no\">\r\n";
+  page += "<meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\"> \r\n";
+  page += "<meta name=\"language\" content=\"de\"> \r\n";
+  page += "<meta http-equiv=\"refresh\" content=\"";
+  page += String(refreshInterval) + "\">\r\n";
+  page += "<style type=\"text/css\">\r\n";
+  page += "  html {font-family: Helvetica; display: inline-block; ";
+  page +=  "margin: 0px auto; text-align: left;}\r\n";
+  page += "  body {background-color: #FFC;margin-top: 50px; margin-left: 20px;}\r\n";
+  page += "  h1 {color: #33C; margin: 20px auto 30px;}\r\n";
+  page += "  p {font-size: 24px; color: #933; margin-bottom: 10px;}\r\n";
+  page += "</style>\r\n";
+  page += "</head>\r\n";
+  page += "<body>\r\n";
+  page += "<div id=\"webpage\">\r\n";
+  page += "<h1>" + title + "</h1> \r\n";
+  page += "<p>Local Access Point SSID = " + String(mySsid) +
+          ", IP address = " + localIP_s +
+          ", MAC address = " + macAddress_s + "</p> \r\n";
+  page +=  "<p>External WLAN SSID = " + String(extSsid);
+  if (wlanOK) {
+    page += ", IP address = " + externalIP_s + "</p> \r\n";
+  } else {
+    page += ", Timeout after " + String(wlanTimeout) + " s</p> \r\n";
+  }
+  page += formatSensorData(true);
+  page += "</div>\r\n";
+  page += "</body>\r\n";
+  page += "</html>\r\n";
+  return page;
+}  //  buildHtml()
+
+
+void setup() {                                      // setup code, to run once
   // Debug Serial
   Serial.begin(115200);
-  while (! Serial);                                 // wait for Debug Serial
+  while (! Serial) ;                                // wait for Debug Serial
   delay(1000);
-  Serial.print("Michaels Raumklima Monitor\n");
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.println();
+  Serial.println(title);
+  Serial.printf("Ikea Vindriktning on Pin %d\n", PIN_UART_RX);
+  // Software Serial für IKEA Sensor
+  sensorSerial.begin(9600);
+  pinMode(ONE_WIRE_BUS, INPUT);
   if (oneWire.reset()) {
     Serial.printf("oneWire on Pin %d\n", ONE_WIRE_BUS);
   } else {
     Serial.printf("No devices found on Pin %d\n", ONE_WIRE_BUS);
   }
-    blink();                                        // when setup finished
-}
+  //  **** This does not work!
+//  oneWire.reset_search();
+//  while (oneWire.search(tempAddr, true)) {
+//    Serial.println("Located Device #" + String(deviceCount) +
+//                   ", ROM-Address = " + deviceAddressToString(tempAddr));
+//    if (ds1820.validFamily(tempAddr)) {
+//      if (dallasCount == 0) {
+//        std::copy(std::begin(tempAddr), std::end(tempAddr), std::begin(ds1820_0));
+//      } else {
+//        std::copy(std::begin(tempAddr), std::end(tempAddr), std::begin(ds1820_1));
+//      }
+//      dallasCount += 1;
+//    }
+//    deviceCount += 1;
+//  }  // while
+//  Serial.println("Found " + String(deviceCount) + " Devices of which " +
+//                 String(dallasCount) + " DS18x00 Sensors");
+//  oneWire.reset();
+  dht.begin();
+  ds1820.begin();
+  ds1820.setResolution(9);                  // global resulution
+  if (ds1820.getAddress(ds1820_0, 0)) {
+    Serial.println("Located Device DS18x20 #0, ROM-Address = " + deviceAddressToString(ds1820_0));
+    // ds1820.setResolution(ds1820_0, 11);     // resolutuion for this sensor
+  } else {
+    Serial.println("Device DS18x20 #0 not found, will be set to inactive");
+    sensor[3].active = false;
+  }
+  if (ds1820.getAddress(ds1820_1, 1)) {
+    Serial.println("Located Device DS18x20 #1, ROM-Address = " + deviceAddressToString(ds1820_1));
+    // ds1820.setResolution(ds1820_1, 11);     // resolutuion for this sensor
+  } else {
+    Serial.println("Device DS18x20 #1 not found, will be set to inactive");
+    sensor[4].active = false;
+  }
+  Serial.print("Parasite power is: ");
+  if (ds1820.isParasitePowerMode())
+    Serial.println("ON"); else Serial.println("OFF");
+  //  deviceCount = ds1820.getDeviceCount();
+  //  dallasCount = ds1820.getDS18Count();
+  //  Serial.printf("Found %d OneWire Device(s) \n", deviceCount);
+  //  Serial.printf("Found %d DS18x00 Sensor(s) \n", dallasCount);
+  //  getSensorData();
+  //  printSensorData();
+  Serial.println("Configuring local access point...");
+  //    if (! WiFi.softAPConfig(localIP, localGateway, localNetmask)) {
+  //    Serial.println("AP Config Failed");
+  //  }
+  WiFi.softAP(mySsid);                  // AP will be open
+  // WiFi.softAP(mySsid, myPassword);   // AP will be password protected
+  localIP_s = WiFi.softAPIP().toString().c_str();
+  Serial.printf("Local Access Point SSID = %s, local IP address = %s\n",
+                mySsid, localIP_s.c_str());
+  macAddress_s = WiFi.softAPmacAddress();
+  Serial.printf("MAC address = %s\n", macAddress_s.c_str());
+  connectWiFi();                        // connect to external WLAN
+  webServer.on("/", handle_OnConnect);
+  webServer.begin();
+  Serial.println("HTTP server started.");
+  blink();                             // when setup finished
+}  // setup()
 
-void loop() {                                       // put your main code here, to run repeatedly
+void loop() {                                       // main code, repeatedly
   unsigned long currentMillis = millis();
+  webServer.handleClient();
   if (currentMillis - previousMillis >= 1000 * processInterval) {
     previousMillis = currentMillis;
-    // do some work
+    getSensorData();
+    printSensorData();
+#ifdef DO_BLINK
     blink(5);
+#endif
   }
-}
+} // loop()
