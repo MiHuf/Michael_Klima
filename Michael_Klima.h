68c05830529457e5c6d1b1c126857ad4ae3e0cbc
Michael Hufschmidt <michael@hufschmidt-web.de>
2023-10-02 17:33:55 +0200

    renamed some include files

--- Michael_Klima.h
+++ Michael_Klima.h
@@ -1,7 +1,7 @@
 /*****************************************************************************
    @file:             Michael_Klima.h
    Created:           2021-12-21
-   Last modification: 2023-09-19
+   Last modification: 2023-10-02
    This is part of Michael_Klima.ino
    Author and (C):    Michael Hufschmidt <michael@hufschmidt-web.de>
    License:           https://creativecommons.org/licenses/by-nc-sa/3.0/de/
@@ -46,9 +46,9 @@
   // ***** Lines below may need modification
   // #define DHTTYPE xxx // moved to individual header files
   // include only one of
-  // "privat.h", "_institut.h" (2), _test.h" (3), "_andreas.h" (4), "_neu.h" (5), "_michael.h" (6), _test2.h" (7), ...
+  // "privat.h" (zzz), "_institut.h" (2), "_test3.h" (3), "_andreas.h" (4), "_test5.h" (5), "_wohnzimmer.h" (6), "_dach.h" (7), ...
   // (x) is the number of the controller, files "_xxx.h" will not be included in the github repository.
-  #include "_michael.h"
+  #include "privat.h"
   #ifndef TOPIC
     #define TOPIC "zuHause"
   #endif
