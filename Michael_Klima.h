975626b3707aa0e4e61865d34465e6422e50502a
Michael Hufschmidt <michael@hufschmidt-web.de>
2023-10-25 12:00:29 +0200

    added option for  _test1.h

--- Michael_Klima.h
+++ Michael_Klima.h
@@ -1,7 +1,7 @@
 /*****************************************************************************
    @file:             Michael_Klima.h
    Created:           2021-12-21
-   Last modification: 2023-10-02
+   Last modification: 2023-10-25
    This is part of Michael_Klima.ino
    Author and (C):    Michael Hufschmidt <michael@hufschmidt-web.de>
    License:           https://creativecommons.org/licenses/by-nc-sa/3.0/de/
@@ -46,7 +46,7 @@
   // ***** Lines below may need modification
   // #define DHTTYPE xxx // moved to individual header files
   // include only one of
-  // "privat.h" (zzz), "_institut.h" (2), "_test3.h" (3), "_andreas.h" (4), "_test5.h" (5), "_wohnzimmer.h" (6), "_dach.h" (7), ...
+  // "privat.h" (zzz), "_test1.h" (1), "_institut.h" (2), "_test3.h" (3), "_andreas.h" (4), "_test5.h" (5), "_wohnzimmer.h" (6), "_dach.h" (7), ...
   // (x) is the number of the controller, files "_xxx.h" will not be included in the github repository.
   #include "privat.h"
   #ifndef TOPIC
